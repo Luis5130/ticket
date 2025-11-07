395d4b83b3a93aad8a322ff37be266ebc576d2e1
Luis5130 <luis.lima@petlove.com.br>
2025-11-07 13:59:56 -0300

    Update 1_📍_Regiões.py

--- pages/1_📍_Regiões.py
+++ pages/1_📍_Regiões.py
@@ -1,20 +1,14 @@
 import streamlit as st
-
 st.title("📍 Desempenho por Região")
-
 df = st.session_state["df_filtrado"]
-
 regioes = df.groupby("regiao", as_index=False).agg({
     "gmv": "sum",
     "necessidades": "sum",
     "convertidas": "sum"
 })
-
 regioes["conversao"] = regioes["convertidas"] / regioes["necessidades"]
 regioes = regioes.fillna(0)
-
 cols = st.columns(3)
-
 for i, row in regioes.iterrows():
     col = cols[i % 3]
     with col:
